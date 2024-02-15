7a93be5d6a9180790fc75197c066211945313e68
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-02-15 23:11:50 +0700

    docs: add comments to apply function

--- src/macten.cpp
+++ src/macten.cpp
@@ -1,6 +1,11 @@
 #include "macten.hpp"
 #include "token_stream.hpp"
 
+/**
+ * Apply declarative template macro on the target token stream. This function returns the succcess status of the macro expansion.
+ * Note that the expansion is recursive, which is why we need to pass in MactenWriter pointer so we can further expand the contents of our macro expansion.
+ * TODO: Better error diagnostics would be great.
+ */
 auto DeclarativeTemplate::apply(
      MactenWriter* env,
      const std::string& indentation,
@@ -10,6 +15,7 @@ auto DeclarativeTemplate::apply(
  {
    using TokenType = MactenAllToken;
 
+   // Arity issue.
    if (args.size() != m_arguments.size()) return false;
 
    auto view = m_token_stream.get_view();
@@ -49,7 +55,6 @@ auto DeclarativeTemplate::apply(
       std::cerr << "Failed to substitute argument name '" << argname << "'\n";
       return false;
      }
-
      continue;
     }
 
