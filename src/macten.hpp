5ba1560869e47b6afdd6bb625f282f194493bab8
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-03-07 14:16:14 +0700

    refactor: moved declarative definition parsing into function

--- src/macten.hpp
+++ src/macten.hpp
@@ -447,6 +447,96 @@ class DeclarativeMacroParser : public cpp20scanner::BaseParser<MactenTokenScanne
    return {std::move(macro_args), std::move(macro_tokens)};
   }
  
+
+  /**
+   * Parse declarative macros.
+   */
+  auto declarative_definition() -> void 
+  {
+     consume(MactenToken::Identifier, "Expected macro name, found: " + previous.lexeme + ".");
+     const auto macro_name = previous.lexeme;
+
+     consume(Token::LBrace, "Expected macro body, missing '{', found: '" + previous.lexeme + "'.");
+
+
+     std::vector<std::string>               branch_bodies;
+     std::vector<DeclarativeMacroParameter> branch_parameters;
+
+     while (!match(Token::RBrace))
+     {
+      const auto parameter_signature = this->scanner.scan_body(Token::LParen, Token::RParen);
+      // std::cout << "[PARSE - SIGNATURE] " << parameter_signature.lexeme << '\n';
+      const auto parameter_signature_stream = macten::TokenStream<MactenToken>::from_string(parameter_signature.lexeme);
+      const auto parameter_signature_view = parameter_signature_stream.get_view();
+      advance();
+   
+      branch_parameters.emplace_back(parameter_signature_view);
+
+      consume(Token::RParen, "Expected arguments, missing ')', found: '" + current.lexeme + "'.");
+   
+
+      std::vector<std::string> macro_args {};
+      std::vector<MactenToken> macro_tokens {};
+
+      consume(Token::Equal, "Expected '=', found: '" + current.lexeme + "'.");
+      consume(Token::GreaterThan, "Expected '>'");
+
+      if (check(Token::LBrace))
+      {
+       scanner.skip_whitespace();
+       auto macro_body_token = this->scanner.scan_body(Token::LBrace, Token::RBrace);
+
+       // std::cout << "[DEBUG]: " << macro_body_token.lexeme << '\n';
+
+       auto token_stream = macten::TokenStream<MactenAllToken>::from_string(macro_body_token.lexeme);
+       auto token_stream_view = token_stream.get_view();
+
+       macten::TokenStream<MactenAllToken> token_stream_result;
+
+       // std::cout << "[DEBUG] WOW\n";
+       while (!token_stream_view.is_at_end())
+       {
+        const auto token = token_stream_view.pop();
+
+        if (token.is(MactenAllToken::Newline))
+        {
+          // Optional skip twice to take into account the indentation of the macro body.
+          // WARN: At the moment, this would break if the prefix spacing is inconsistent.
+          // WARN: If you are using an editor which converts tabs into multiple spaces,
+          //       the result of the macro body will be incorrect.
+          // TODO: Work out a better scheme and fix this. One possibility is to book mark the 
+          //       indentation characters used before the first line of the body and of the macro.
+          //       this simply skip the book marked amount when scanning subsequent lines. This solution
+          //       is not exactly idea either because there might be cases where the user might want to
+          //       write a simple single line macro. Perhaps a logging system should be implemented and
+          //       emit a warning. This would work great because currently the project is lacking logging
+          //       and error reporting capabilities.
+          static_cast<void>(token_stream_view.consume(MactenAllToken::Tab, MactenAllToken::Space));
+          static_cast<void>(token_stream_view.consume(MactenAllToken::Tab, MactenAllToken::Space));
+        }
+
+        token_stream_result.push_back(token);
+       }
+
+       if (token_stream_result.peek_back().is(MactenAllToken::Newline)) 
+       {
+        token_stream_result.pop_back();
+       }
+   
+       branch_bodies.emplace_back(token_stream_result.construct());
+
+       advance();
+       advance();
+       }
+
+     }
+
+      m_macros.emplace_back(
+       std::move(macro_name), 
+       std::move(branch_bodies), 
+       std::move(branch_parameters));
+  } 
+
   /**
    * Parse declartions.
    */
@@ -454,88 +544,7 @@ class DeclarativeMacroParser : public cpp20scanner::BaseParser<MactenTokenScanne
   {
    if (match(Token::DeclarativeDefinition))
    {
-    consume(MactenToken::Identifier, "Expected macro name, found: " + previous.lexeme + ".");
-    const auto macro_name = previous.lexeme;
-
-    consume(Token::LBrace, "Expected macro body, missing '{', found: '" + previous.lexeme + "'.");
-
-
-    std::vector<std::string>               branch_bodies;
-    std::vector<DeclarativeMacroParameter> branch_parameters;
-
-    while (!match(Token::RBrace))
-    {
-     const auto parameter_signature = this->scanner.scan_body(Token::LParen, Token::RParen);
-     const auto parameter_signature_stream = macten::TokenStream<MactenToken>::from_string(parameter_signature.lexeme);
-     const auto parameter_signature_view = parameter_signature_stream.get_view();
-     advance();
-    
-     branch_parameters.emplace_back(parameter_signature_view);
-
-     consume(Token::RParen, "Expected arguments, missing ')', found: '" + current.lexeme + "'.");
-    
-
-     std::vector<std::string> macro_args {};
-     std::vector<MactenToken> macro_tokens {};
-
-     consume(Token::Equal, "Expected '=', found: '" + current.lexeme + "'.");
-     consume(Token::GreaterThan, "Expected '>'");
-
-     if (check(Token::LBrace))
-     {
-      scanner.skip_whitespace();
-      auto macro_body_token = this->scanner.scan_body(Token::LBrace, Token::RBrace);
-
-
-      auto token_stream = macten::TokenStream<MactenAllToken>::from_string(macro_body_token.lexeme);
-      auto token_stream_view = token_stream.get_view();
-
-      macten::TokenStream<MactenAllToken> token_stream_result;
-
-      while (!token_stream_view.is_at_end())
-      {
-       const auto token = token_stream_view.pop();
-
-       if (token.is(MactenAllToken::Newline))
-       {
-         // Optional skip twice to take into account the indentation of the macro body.
-         // WARN: At the moment, this would break if the prefix spacing is inconsistent.
-         // WARN: If you are using an editor which converts tabs into multiple spaces,
-         //       the result of the macro body will be incorrect.
-         // TODO: Work out a better scheme and fix this. One possibility is to book mark the 
-         //       indentation characters used before the first line of the body and of the macro.
-         //       this simply skip the book marked amount when scanning subsequent lines. This solution
-         //       is not exactly idea either because there might be cases where the user might want to
-         //       write a simple single line macro. Perhaps a logging system should be implemented and
-         //       emit a warning. This would work great because currently the project is lacking logging
-         //       and error reporting capabilities.
-         static_cast<void>(token_stream_view.consume(MactenAllToken::Tab, MactenAllToken::Space));
-         static_cast<void>(token_stream_view.consume(MactenAllToken::Tab, MactenAllToken::Space));
-       }
-
-       token_stream_result.push_back(token);
-      }
-
-      if (token_stream_result.peek_back().is(MactenAllToken::Newline)) 
-      {
-       token_stream_result.pop_back();
-      }
-    
-      branch_bodies.emplace_back(token_stream_result.construct());
-
-     // std::cout << "\nMacro body:\n===================================================\n";
-     // std::cout << macro_body;
-     // std::cout << "\n===================================================\n";
-      advance();
-      advance();
-    }
-
-    }
-
-     m_macros.emplace_back(
-      std::move(macro_name), 
-      std::move(branch_bodies), 
-      std::move(branch_parameters));
+     declarative_definition();
    }
    else
    {
