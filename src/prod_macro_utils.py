f9fe8ba49b524d075c8b467ea15b27c5d560301c
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-05-02 18:48:50 +0700

    feat: proc macro python util list stream

--- src/prod_macro_utils.py
+++ src/prod_macro_utils.py
@@ -1,3 +1,30 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class ListStream:
+    lst: List[str]
+
+    def pop_if(self, match):
+        v = self.lst[0]
+
+        if v == match:
+            self.pop()
+            return True
+
+        return False
+
+    def at(self, idx):
+        return self.lst[idx]
+
+    def peek(self, idx=0):
+        return self.lst[idx]
+
+    def pop(self, idx=0):
+        return self.lst.pop(idx)
+
+
 class ProceduralMacroContext:
     def __init__(self):
 
